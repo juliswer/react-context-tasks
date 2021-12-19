fe3909602e4c736f83853a81744e80308941f93c
Julian Swerdlin <julianswer36@gmail.com>
2021-12-19 23:57:15 +0100

    updating files

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,12 +3,24 @@ import {GlobalContext} from '../context/GlobalContext';
 
 const TaskList = () => {
 
-    const contexto = useContext(GlobalContext);
-    console.log(contexto);
+    const {tasks} = useContext(GlobalContext);
 
     return (
-        <div>
-            Task List
+        <div className="flex justify-center">
+            <div className="w-6/12">
+                {tasks.map((task) => (
+                    <div className="bg-gray-900 px-20 py-5 text-white shadow-2x1 mb-4 flex justify-between">
+                        <div>
+                            <h1>{task.title}</h1>
+                            <h5>{task.id}</h5>
+                        </div>
+                        <div>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,6 +1,18 @@
 import {createContext} from 'react';
 
 export const GlobalContext = createContext({
-    tasks: [],
-    done: true
+    tasks: [
+        {
+            id: "1",
+            title: 'title one',
+            description: 'some description',
+            done: false
+        },
+        {
+            id: "2",
+            title: 'title two',
+            description: 'some description 2',
+            done: false
+        }
+    ],
 });
