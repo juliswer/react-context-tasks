f221a6922f75aadac12109253b9aebe892b204cd
Julian Swerdlin <julianswer36@gmail.com>
2021-12-19 23:58:54 +0100

    updating files

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -9,7 +9,7 @@ const TaskList = () => {
         <div className="flex justify-center">
             <div className="w-6/12">
                 {tasks.map((task) => (
-                    <div className="bg-gray-900 px-20 py-5 text-white shadow-2x1 mb-4 flex justify-between" key={task}>
+                    <div className="bg-gray-900 px-20 py-5 text-white shadow-2x1 mb-4 flex justify-between" key={task.id}>
                         <div>
                             <h1>{task.title}</h1>
                             <h5>{task.id}</h5>
