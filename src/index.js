0068fbf777b55742f7a47532fdf258e5c257afa8
Julian Swerdlin <julianswer36@gmail.com>
2021-12-19 23:18:17 +0100

    updating files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom';
-import '../tailwind.css'
+import './tailwind.css'
 import App from './App';
 
 
