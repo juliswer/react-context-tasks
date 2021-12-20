79f3c7ee6096a4c70885bc7df1ed2e3424a4b871
Julian Swerdlin <julianswer36@gmail.com>
2021-12-20 16:48:57 +0100

    front fiels

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const TaskForm = () => {
-    return (
-        <div>
-            Task form
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center items-center h-3/4">
+      <form className="bg-gray-900 p-10">
+        <h2 className="mb-7 text-3x1">Add a Task</h2>
 
-export default TaskForm
+        <div className="mb-5">
+          <input
+            type="text"
+            className="py-3 px-4 focus: outline-none focus:text-gray-100 bg-gray-700 w-full"
+            name="title"
+            placeholder="Write a title"
+          ></input>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default TaskForm;
